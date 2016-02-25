39dbaafafad9149bf66d174d195a89bc4b01ad9c
Daniel Howard <dannyman@toldme.com>
2016-02-24 16:44:31 -0800

    report number of map and reduces launched

--- metrics-report.pl
+++ metrics-report.pl
@@ -42,25 +42,35 @@ foreach my $job_ref (@jobs) {
 	my $time_submitted = $job_ref->{Time_Submitted};
 	my $time_started = $job_ref->{Time_Started};
 	my $time_finished = $job_ref->{Time_Finished};
+	my $maps_rack_local = 0;
+	my $maps_total_launched = 0;
+	my $reduces_total_launched = 0;
 
 	# See: http://answers.mapr.com/answers/167354/view.html
 	$job_name =~ s/^\[\w+\/\w+\]//; # Trim [blahblah/blahblah]
 
 	printf "\nJOB: %s %s\n", $job, $job_name;
-	printf "%-40s %10s %6s\n", "SUBMITTED AT:",
+	printf "%-40s %10s  %5s\n", "SUBMITTED AT:",
 		strftime("%Y-%m-%d", localtime($time_submitted/1000)),
 		strftime("%H:%M", localtime($time_submitted/1000));
-	printf "%-40s %10s %6s %6s\n", "TOTAL TIME:",
+	printf "%-40s %10s  %5s %5s\n", "TOTAL TIME:",
 		concise(duration($duration_ms/1000)),
 		strftime("%H:%M", localtime($time_started/1000)),
 		strftime("%H:%M", localtime($time_finished/1000));
+
+	$maps_rack_local = $dbh->selectrow_array("select ATTR_VALUE from JOB_ATTRIBUTES where JOB_ID='$job' and ATTR_NAME = 'RACK_LOCAL_MAPS'") || 0;
+	$maps_total_launched = $dbh->selectrow_array("select ATTR_VALUE from JOB_ATTRIBUTES where JOB_ID='$job' and ATTR_NAME = 'TOTAL_LAUNCHED_MAPS'") || 0;
+	$reduces_total_launched = $dbh->selectrow_array("select ATTR_VALUE from JOB_ATTRIBUTES where JOB_ID='$job' and ATTR_NAME = 'TOTAL_LAUNCHED_REDUCES'") || 0;
 
 	$sth = $dbh->prepare("select JOB_ID,AVG(TIME_FINISHED - TIME_STARTED) as 'MS',STD(TIME_FINISHED - TIME_STARTED) as 'STD' from TASK_ATTEMPT where JOB_ID = '$job' and TYPE = 'MAP' and STATUS='SUCCEEDED' group by JOB_ID");
 	$sth->execute();
 	while( my $ref = $sth->fetchrow_hashref() ) {
 		$avg = $ref->{'MS'};
 		$std = $ref->{'STD'};
-		printf "%-40s %10s\n", "MAP AVERAGE:", concise(duration($avg/1000));
+#		printf "%-40s %10s  %d/%d local/launched\n", "MAP AVERAGE:",
+#			concise(duration($avg/1000)), $maps_rack_local, $maps_total_launched;
+		printf "%-40s %10s %6d launched\n", "MAP AVERAGE:",
+			concise(duration($avg/1000)), $maps_rack_local, $maps_total_launched;
 	}
 
 	my $rs = $dbh->selectall_arrayref("select JOB_ID,HOST,AVG(TIME_FINISHED - TIME_STARTED) as 'MS' from TASK_ATTEMPT where JOB_ID = '$job' and TYPE = 'MAP' and STATUS='SUCCEEDED' group by HOST order by MS");
@@ -79,7 +89,8 @@ foreach my $job_ref (@jobs) {
 	while( my $ref = $sth->fetchrow_hashref() ) {
 		$avg = $ref->{'MS'};
 		$std = $ref->{'STD'};
-		printf "%-40s %10s\n", "REDUCE AVERAGE:", concise(duration($avg/1000));
+		printf "%-40s %10s %6d launched\n", "REDUCE AVERAGE:",
+			concise(duration($avg/1000)), $reduces_total_launched;
 	}
 
 	$rs = $dbh->selectall_arrayref("select JOB_ID,HOST,AVG(TIME_FINISHED - TIME_STARTED) as 'MS' from TASK_ATTEMPT where JOB_ID = '$job' and TYPE = 'REDUCE' and STATUS='SUCCEEDED' group by HOST order by MS");
